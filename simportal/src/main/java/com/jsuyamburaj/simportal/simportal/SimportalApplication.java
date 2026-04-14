b36468fac7938228e07cb353a94f5029aecaffd0
jsuyamburaj <jsuyamburaj@gmail.com>
2026-04-14 07:31:19 +0530

    java setup is complete

---
+++ simportal/src/main/java/com/jsuyamburaj/simportal/simportal/SimportalApplication.java
@@ -0,0 +1,13 @@
+package com.jsuyamburaj.simportal.simportal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimportalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimportalApplication.class, args);
+	}
+
+}
